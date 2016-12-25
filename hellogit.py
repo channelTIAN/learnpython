615a80d6918d639336c71d19a43ac6fcf0ef3fd2
tian.hongzhong <chinatianhongzhong@gmai.com>
2016-12-25 18:53:44 +0800

    this is my first excuted python file in my git

---
+++ hellogit.py
@@ -0,0 +1,10 @@
+# -*- conding: utf-8 -*-
+
+def SayHello(name):
+	print("hello,", name)
+	print("this is your first git program\nWelcome!!")
+
+if __name__ == "__main__":
+	print("Please input your name:")
+	name = input()
+	SayHello(name)
