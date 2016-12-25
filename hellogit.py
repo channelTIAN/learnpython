538ec9f7aac64b89eefe2dbebf2779440de6bcc8
tian.hongzhong <chinatianhongzhong@gmail.com>
2016-12-25 18:59:39 +0800

    add congratulate

--- hellogit.py
+++ hellogit.py
@@ -3,8 +3,12 @@
 def SayHello(name):
 	print("hello,", name)
 	print("this is your first git program\nWelcome!!")
+	
+def Congratulate(name):
+	print(name, "Congratulate!!")
 
 if __name__ == "__main__":
 	print("Please input your name:")
 	name = input()
 	SayHello(name)
+	Congratulate(name)
